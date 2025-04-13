1ef8fa980b3ccb674125b4ffafcfce334213dcce
Zara1994 <avetisyan.zarine1994@gmail.com>
2025-04-13 17:48:41 +0500

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
 //            CategorySeeder::class,
 //            ProductSeeder::class,
-            ProjectsSeeder::class
+//            ProjectsSeeder::class
+            ReviewsSeeder::class
         ]);
     }
 }
---
+++ database/seeders/ReviewsSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ReviewsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Получаем все идентификаторы категорий
+
+
+        // Вставляем данные в таблицу products
+        DB::table('reviews')->insert([
+            [
+                'name' => 'user one',
+                'text' => 'review description text',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'user 2',
+                'description' => 'review description text',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'project 3',
+                'description' => 'project description',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'project 4',
+                'description' => 'project description',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+        ]);
+    }
+}
